9ba31d64e9b08d65da34e6d0005a05921e664e90
Bonniu <radekg23@o2.pl>
2020-04-28 17:21:02 +0200

    spliting predicted 1d array into 2d array, counting error

--- Zadanie2/Main.py
+++ Zadanie2/Main.py
@@ -1,8 +1,37 @@
+from math import sqrt
+
 from sklearn.neural_network import MLPRegressor
 
 from FileReader import FileReader
 
 nr_of_records = 1540
+
+
+def find_solution_and_errors(predicted: [], reference: []):
+    errors = []
+    for i in range(len(predicted)):
+        tmp_x = reference[i][0] - predicted[i][0]
+        tmp_y = reference[i][1] - predicted[i][1]
+        errors.append(sqrt(tmp_x ** 2 + tmp_y ** 2))
+        print(i, "predicted:", predicted[i], "reference: ", reference[i], " error:", errors[i])
+    return errors
+
+
+def convert_to_2d_array(predicted_1d: []):
+    result = []
+    for i in range(len(predicted_1d)):
+        tmp = str(predicted_1d[i])[1:-1].split(" ")  # [1975.55791897   4168.3236049 ] - przykladowy element tablicy
+        remove_empty_elements(tmp)
+        result.append([float(tmp[0]), float(tmp[1])])
+    return result
+
+
+def remove_empty_elements(tab: []):
+    while tab.__len__() > 2:
+        try:
+            tab.remove('')
+        except ValueError:
+            pass
 
 
 if __name__ == "__main__":
@@ -18,10 +47,12 @@ if __name__ == "__main__":
                        max_iter=200, shuffle=True, tol=1, verbose=False, early_stopping=False)
 
     print(mlp.__repr__())
+
     mlp.fit(train, train_ref)
-    # print(test)
-    print(mlp.predict(test))  # sprawdzenie
-    print(mlp.predict(test).__len__())  # sprawdzenie
+    predicted_2d_array = convert_to_2d_array(mlp.predict(test))
+    find_solution_and_errors(predicted_2d_array, test_ref)
+
+    print("-------------------------------")
     print(mlp.predict(test)[0])
     print(mlp.predict(test)[13])
     print(mlp.predict(test)[22])
